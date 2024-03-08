7aa00fc19b3beefd7cd3ffa1b88c4581cf7d9ae4
Hancie123 <hanciewanemphago@gmail.com>
2024-03-08 21:17:51 +0545

    design updated

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,7 +8,13 @@
         image="{{ url('assets/img/logo.png') }}" />
 
 
-    <link rel="icon" href="{{ url('assets/img/logo.png') }}">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('assets/img/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url('assets/img/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('assets/img/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ url('assets/img/site.webmanifest')}}">
+
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
