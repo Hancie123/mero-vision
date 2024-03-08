b529729eeca0b6d5b98bc49bec3e3872243351f5
Hancie123 <hanciewanemphago@gmail.com>
2024-03-08 18:00:00 +0545

    design updated

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,13 +2,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <x-meta title="Mero Vision" 
-    card="summary"
-    description="Blade components are awesome!" 
-    image="{{url('assets/img/logo.png')}}" />
+    <x-meta title="Mero Vision"
+        card="Welcome to Mero Vision's website! We are a premier IT company based in Kathmandu, Nepal, dedicated to providing innovative solutions to meet your digital needs. Specializing in Android and iOS application development, website design, and web application development, we strive to deliver cutting-edge solutions tailored to your unique requirements."
+        description="Welcome to Mero Vision's website! We are a premier IT company based in Kathmandu, Nepal, dedicated to providing innovative solutions to meet your digital needs. Specializing in Android and iOS application development, website design, and web application development, we strive to deliver cutting-edge solutions tailored to your unique requirements."
+        image="{{ url('assets/img/logo.png') }}" />
 
-    
-    <link rel="icon" href="{{url('assets/img/logo.png')}}">
+
+    <link rel="icon" href="{{ url('assets/img/logo.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
